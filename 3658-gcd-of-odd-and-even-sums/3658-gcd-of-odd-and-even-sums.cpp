ea271cc57720bbed44e44f882b604a7772511a04
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2026-07-15 10:56:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (9.11%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // sum of first n even numbers = (n*n)
+        // sum of first n odd numbers = (n*(n+1))
+        // gcd is n
+        return n;
+    }
+};
