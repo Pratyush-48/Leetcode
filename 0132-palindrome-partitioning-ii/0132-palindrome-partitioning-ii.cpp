82f3cbb61408f79d74fa39afa89a752e8e02ad1f
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2026-07-19 12:53:24 +0530

    Time: 141 ms (56.55%), Space: 63 MB (8.88%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     int n;
-    vector<vector<bool>>pali;
-    vector<vector<int>>dp;
-    bool recc(int i,int j,string &s){
+    vector<vector<int>>pali;
+    bool p(int i,int j,string &s){
         if(i>j) return true;
-        if(dp[i][j]!=-1) return dp[i][j];
-        bool check = s[i]==s[j] && recc(i+1,j-1,s);
-        pali[i][j] = check;
-        recc(i+1,j,s);
-        recc(i,j-1,s);
-        return dp[i][j] = check;
+        if(pali[i][j]!=-1) return pali[i][j];
+        bool take = s[i]==s[j] && p(i+1,j-1,s);
+        pali[i][j] = take;
+        p(i+1,j,s);
+        p(i,j-1,s);
+        return take;
     }
-    vector<int>t;
-    int cut(int i,string&s){
-        if(i>=n) return 0;
-        if(t[i]!=-1) return t[i];
+    vector<int>dp;
+    int cuts(int idx,string &s){
+        if(idx>=n) return 0;
+        if(dp[idx]!=-1) return dp[idx];
         int mini = INT_MAX;
-        for(int j=i;j<n;j++){
-            if(pali[i][j]){
-                int curr = 1+cut(j+1,s);
+        for(int j=idx;j<n;j++){
+            if(pali[idx][j]){
+                int curr = 1+cuts(j+1,s);
                 mini = min(mini,curr);
             }
         }
-        return t[i] = mini;
+        return dp[idx] = mini;
     }
     int minCut(string s) {
         n = s.length();
-        pali.resize(n,vector<bool>(n,false));
-        dp.resize(n,vector<int>(n,-1));
-        recc(0,n-1,s);
-        t.resize(n,-1);
-        return cut(0,s)-1;
+        pali.resize(n,vector<int>(n,-1));
+        dp.resize(n,-1);
+        p(0,n-1,s);
+        return cuts(0,s)-1;
     }
 };
