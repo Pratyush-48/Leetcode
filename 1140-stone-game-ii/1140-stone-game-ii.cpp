35c31de16fc51b02a7f9161aff507de01819f082
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2026-08-09 17:48:47 +0530

    Time: 10 ms (58.4%), Space: 11.8 MB (72.97%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int n;
-    int dp[101][101];
-    int recc(int idx,int m,vector<int>&piles,vector<int>&pref){
+    int dp[101][101][2];
+    int recc(int idx,vector<int>&piles,int turn,int m,vector<int>&pref){
         if(idx>=n) return 0;
-        if(dp[idx][m]!=-1) return dp[idx][m];
-        int maxi = INT_MIN;
-        int N = min(n, idx + 2 * m);
-        for(int i=idx;i<N;i++){
-            int curr = pref[i]-pref[idx]+piles[idx];
-            int rem = i-idx+1;
-            int next_m = max(m,rem);
-            int mini = INT_MAX;
-            if(i+1>=n) mini = 0;
-            int bob_limit = min(n, (i + 1) + 2 * next_m);
-            for(int j=i+2;j<=bob_limit;j++){
-                int y = j-i-1;
-                mini = min(mini,recc(j,max(next_m,y),piles,pref));
-            }
-            maxi = max(maxi,curr+mini);
+        if(dp[idx][m][turn]!=-1) return dp[idx][m][turn];
+        int result = INT_MAX;
+        if(turn==1){
+            result = 0;
         }
-        return dp[idx][m]=maxi;
+        for(int x=1;x<=(2*m);x++){
+            if(idx + x - 1 >= n) break;
+            if(turn==1){
+                result = max(result,(pref[idx+x-1]-pref[idx]+piles[idx])+recc(idx+x,piles,0,max(m,x),pref));
+            }
+            else{
+                result = min(result,recc(idx+x,piles,1,max(m,x),pref));
+            }
+        }
+        return dp[idx][m][turn]=result;
     }
     int stoneGameII(vector<int>& piles) {
         n = piles.size();
         vector<int>pref(n,0);
         pref[0] = piles[0];
         for(int i=1;i<n;i++){
-            pref[i] = piles[i] + pref[i-1];
+            pref[i] = (pref[i-1]+piles[i]);
         }
         memset(dp,-1,sizeof(dp));
-        return recc(0,1,piles,pref);
+        return recc(0,piles,1,1,pref);
     }
 };
